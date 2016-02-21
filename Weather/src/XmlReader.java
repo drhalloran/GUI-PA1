4fb26f8d54a2d6c5151c72c7ba00bf6753e518c8
drhalloran <drhalloran@gmail.com>
2016-02-20 17:50:26 -0700

    added comments.

--- Weather/src/XmlReader.java
+++ Weather/src/XmlReader.java
@@ -1,11 +1,8 @@
 
-import java.awt.List;
+
 import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.HashMap;
-import java.util.Map;
 import org.w3c.dom.*;
-import org.xml.sax.*;
 import javax.xml.parsers.*;
 
     /*
@@ -53,31 +50,37 @@ public class XmlReader {
             for ( int i = 0; i < nlist.getLength(); i++ )
             {
                 String nlistName = nlist.item(i).getNodeName();
+                //Check to make sure top xml node is weather or #text
                 if(!nlistName.equals("weather") && !nlistName.equals("#text"))
                 {
                     throw new Exception("Missing weather tag in XML");
                 }
                 // iterate through grandchild nodes
                 NodeList mlist = nlist.item( i ).getChildNodes();
+                //temporary variables used for combining date and time
                 String dateTemp = "";
                 String timeTemp = "";
                 for ( int j = 0; j < mlist.getLength(); j++ )
                 {
+                    //get current node name (date, humidty, temp, etc)
                     String mlistName = mlist.item(j).getNodeName();
+                    //skip any text nodes
                     if(!mlistName.equals("#text"))
                     {
                         NodeList klist = mlist.item( j ).getChildNodes();
                         if ( klist.getLength() > 0 )
                         {
-                            //System.out.println( mlist.item( j ).getNodeName() + " = " + klist.item( 0 ).getNodeValue() );
-                            
+                            //Set correct data based on current xml node
+                            //klist.item(0).getNodeValue() will give the value of the current xml node
                             switch(mlistName)
                             {
                                 case "date":
+                                    //if timeTemp hasn't been set just store the date to the temp variable
                                     if(timeTemp.equals(""))
                                     {
                                         dateTemp = klist.item(0).getNodeValue();
                                     }
+                                    //If both time and date are available, combine strings and store to date object
                                     else
                                     {
                                         SimpleDateFormat dateFormatter = new SimpleDateFormat ("MM/dd/yy-h:mma");
@@ -85,10 +88,12 @@ public class XmlReader {
                                     }                                    
                                     break;
                                 case "time":
+                                    //if dateTemp hasn't been set just store the date to the temp variable
                                     if(dateTemp.equals(""))
                                     {
                                         timeTemp = klist.item(0).getNodeValue().trim() + "M";
                                     }
+                                    //If both time and date are available, combine strings and store to date object
                                     else
                                     {
                                         SimpleDateFormat dateFormatter = new SimpleDateFormat ("MM/dd/yy-h:mma");
